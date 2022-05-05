fcdf2ddc3149aa2d5b558761cb401d13ede78b0a
Enes Yalçın <enesylcn57@outlook.com>
2022-05-05 14:31:15 +0300

    Add project files.

---
+++ Skill_CodeFirstEntity/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirstEntity.Models.Sınıflar;
+
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        CONTEXT c = new CONTEXT();
+        public ActionResult Index()
+        {
+            
+            var degerler = c.YETENEKLERS.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(YETENEKLER y)
+        {
+            
+            c.YETENEKLERS.Add(y);
+            c.SaveChanges();
+            return View();
+        }
+        public ActionResult YetenekSil(int id)
+        {
+
+           var deger= c.YETENEKLERS.Find(id);
+            c.YETENEKLERS.Remove(deger);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekGuncelle(int id)
+        {
+            var deger = c.YETENEKLERS.Find(id);
+
+            return View("YetenekGuncelle", deger);
+        }
+        [HttpPost]
+        public ActionResult YetenekGuncelle(YETENEKLER y)
+        {
+            var deger = c.YETENEKLERS.Find(y.ID);
+            deger.ACIKLAMA = y.ACIKLAMA;
+            deger.DEGER = y.DEGER;
+            c.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Skill_CodeFirstEntity/Models/Sınıflar/CONTEXT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Skill_CodeFirstEntity.Models.Sınıflar
+{
+    public class CONTEXT:DbContext
+    {
+        public DbSet<YETENEKLER> YETENEKLERS { get; set; }
+    }
+}
